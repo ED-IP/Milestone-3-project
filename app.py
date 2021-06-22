2f501f1ca3672a58e92b2eae38590641754c517b
ED-IP <edip_courses@outlook.es>
2021-06-22 19:15:27 +0000

    removed debug from __main__ in app.py

--- app.py
+++ app.py
@@ -259,5 +259,4 @@ def delete_entry(term):
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
-            debug=False)
+            port=int(os.environ.get("PORT")))
