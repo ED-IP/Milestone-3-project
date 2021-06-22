9470f23fc85fab59c04b0b600713466520421dda
ED-IP <edip_courses@outlook.es>
2021-06-22 12:06:48 +0000

    added character counter to the input text in add_entry

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,5 @@
  $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.collapsible').collapsible();
+    $('input#input_text, textarea#textarea2').characterCounter();
   });
